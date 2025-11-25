7d1e4067eef5d98d6f6a36eaa7c5f5a22fcb4e5b
Antonio Carlos da Silva Barros <antoniocarlos180707@gmail.com>
2025-11-25 14:00:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ st.dataframe(df.drop(columns=["_lat", "_lon"], errors="ignore"), use_container_w
 # ---------------------------------------------------------
 # DOWNLOADS
 # ---------------------------------------------------------
-df_exporta = df.drop(columns=["_lat", "_lon"], errors="ignore")
+df_exporta = df.drop(columns=["_lat", "_lon", lat_col, lon_col],, errors="ignore")
 
 # XLSX
 buffer = io.BytesIO()
@@ -107,5 +107,6 @@ if mapa:
         file_name="mapa_infraestrutura_total.html",
         mime="text/html"
     )
+
 
 
